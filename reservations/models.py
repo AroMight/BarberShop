8f479a984d64c5d0511f1d1388fdd9e17cbeb348
Denilson Silva <162153089+AroMight@users.noreply.github.com>
2024-08-20 19:46:47 -0300

    chore: Update reservations app models and migrations

--- reservations/models.py
+++ reservations/models.py
@@ -9,9 +9,9 @@ class Service(models.Model):
         upload_to='services/covers/%Y/%m/%d', null=True, blank=True, default='')
     price = models.DecimalField(max_digits=5, decimal_places=2)
     duration_time = models.DurationField()
-    status = models.BooleanField(default=True)
+    status = models.BooleanField(default=False)
     updated_at = models.DateTimeField(auto_now=True)
-    barbers = models.ManyToManyField(Employee, related_name='services')
+    barbers = models.ManyToManyField(Employee, related_name='services', blank=True)
 
     def __str__(self):
         return str(self.name)
