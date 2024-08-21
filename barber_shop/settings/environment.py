f727350929f6bba0ea82c0e53a8bd841e82214cf
Denilson Silva <162153089+AroMight@users.noreply.github.com>
2024-08-21 17:01:33 +0000

    enviroment att

--- barber_shop/settings/environment.py
+++ barber_shop/settings/environment.py
@@ -1,11 +1,11 @@
 import os
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = str(os.environ.get('SECRET_KEY'))
 
 
 # Converta a string para booleano
-DEBUG = os.environ.get('DEBUG').lower() in ('true', '1', 't', 'yes', 'y')
+DEBUG = str(os.environ.get('DEBUG')).lower() in ('true', '1', 't', 'yes', 'y')
 
 ALLOWED_HOSTS = ["*"]
 INTERNAL_IPS = [
