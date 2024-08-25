07ec0a1b52d1ab3ad52c5b92497dc8471a867e6e
Denilson Silva <162153089+AroMight@users.noreply.github.com>
2024-08-25 15:50:39 -0300

    feat: Update reservation form and modal for scheduling appointments

--- forms/icon_fields.py
+++ forms/icon_fields.py
@@ -20,3 +20,18 @@ class IconUsernameField(UsernameField):
     def __init__(self, *args, icon=None, **kwargs):
         self.icon = icon
         super().__init__(*args, **kwargs)
+
+class IconDateField(forms.DateField):
+    def __init__(self, *args, icon=None, **kwargs):
+        self.icon = icon
+        super().__init__(*args, **kwargs)
+
+class IconModelChoiceField(forms.ModelChoiceField):
+    def __init__(self, *args, icon=None, **kwargs):
+        self.icon = icon
+        super().__init__(*args, **kwargs)
+
+class IconTimeField(forms.TimeField):
+    def __init__(self, *args, icon=None, **kwargs):
+        self.icon = icon
+        super().__init__(*args, **kwargs)
--- reservations/forms/reservations_forms.py
+++ reservations/forms/reservations_forms.py
@@ -1,25 +1,46 @@
 from django import forms
+from forms.icon_fields import IconDateField, IconModelChoiceField, IconTimeField
 from ..models import Reservation
+from ..models import Service, Branch
 
 
 class ReservationForm(forms.ModelForm):
-    date = forms.DateField(label="Choose a date",widget=forms.DateInput(
-        attrs={'class': 'form-control', 'type': 'date'}))
 
-    available_times = [
-        ('09:00', '09:00'),
-        ('10:00', '10:00'),
-        ('11:00', '11:00'),
-        ('12:00', '12:00'),
-        ('13:00', '13:00'),
-        ('14:00', '14:00'),
-        ('15:00', '15:00'),
-        ('16:00', '16:00'),
-        ('17:00', '17:00'),
-    ]
+    service = IconModelChoiceField(
+        icon='bi bi-bag-fill',
+        queryset=Service.objects.all(),
+        label="Choose a service",
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
 
-    time = forms.ChoiceField(label="Choose the hour",choices=available_times, widget=forms.RadioSelect(
-        attrs={'class': 'row row-cols-2 row-cols-sm-3 row-cols-md-3 row-cols-lg-5'}))
+    date = IconDateField(
+        icon='bi bi-calendar-check-fill',
+        label="Choose a date",
+        widget=forms.DateInput(
+            attrs={'class': 'form-control', 'type': 'date'}
+        )
+    )
+
+    branch = IconModelChoiceField(
+        icon='bi bi-shop-window',
+        queryset=Branch.objects.all(),
+        label="Choose a branch",
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    time = IconTimeField(
+        icon='bi bi-clock-fill',
+        label="Choose the hour",
+        widget=forms.TimeInput(
+            attrs={
+                'class': 'row form-control row-cols-2 row-cols-sm-3 row-cols-md-3 row-cols-lg-5',
+                'type': 'time',
+                'min': '09:00',
+                'max': '17:00',
+                'step': '3600'
+            }
+        )
+    )
 
     class Meta:
         model = Reservation
@@ -29,17 +50,3 @@ class ReservationForm(forms.ModelForm):
             'branch',
             'time',
         ]
-
-        widgets = {
-            'service': forms.Select(attrs={'class': 'form-control'}),
-            'branch': forms.Select(attrs={'class': 'form-control'}),
-        }
-
-        labels = {
-            'service': 'Service',
-            'branch': 'Branch',
-        }
-
-        # list of all configurations such a label, widgets that i can do in class meta
-        # https://docs.djangoproject.com/en/3.2/ref/forms/widgets/
-        # https://docs.djangoproject.com/en/3.2/ref/forms/fields/
