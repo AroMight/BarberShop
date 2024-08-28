f49fc830daeb2d9fad263029635d69dd8d2fb0fb
Denilson Silva <162153089+AroMight@users.noreply.github.com>
2024-08-28 18:09:59 -0300

    chore: Refactor home view and add extra context for Barber Shop template

--- forms/icon_fields.py
+++ forms/icon_fields.py
@@ -21,15 +21,18 @@ class IconUsernameField(UsernameField):
         self.icon = icon
         super().__init__(*args, **kwargs)
 
+
 class IconDateField(forms.DateField):
     def __init__(self, *args, icon=None, **kwargs):
         self.icon = icon
         super().__init__(*args, **kwargs)
 
+
 class IconModelChoiceField(forms.ModelChoiceField):
     def __init__(self, *args, icon=None, **kwargs):
         self.icon = icon
         super().__init__(*args, **kwargs)
+
 
 class IconTimeField(forms.TimeField):
     def __init__(self, *args, icon=None, **kwargs):
