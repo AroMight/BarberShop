2ed60e128bf0e7fa8c39baba5075e4603039a8b7
Denilson <fcodenilson@gmail.com>
2024-09-16 21:24:54 -0300

    Everything OK

--- reservations/models.py
+++ reservations/models.py
@@ -2,17 +2,17 @@ from django.db import models
 from users.models import Employee, Customer, Branch
 
 
-
 class Service(models.Model):
     name = models.CharField(max_length=50)
     # description = models.TextField()
     cover = models.ImageField(
-        upload_to='services/covers/%Y/%m/%d', null=True, blank=True, default='')
+        upload_to="services/covers/%Y/%m/%d", null=True, blank=True, default=""
+    )
     price = models.DecimalField(max_digits=5, decimal_places=2)
     # duration_time = models.DurationField()
     status = models.BooleanField(default=False)
-    is_highlighted = models.BooleanField(default=False, db_comment='Highlight this service in the home page')
-    barbers = models.ManyToManyField(Employee, related_name='services', blank=True)
+    is_highlighted = models.BooleanField(default=False)
+    barbers = models.ManyToManyField(Employee, related_name="services", blank=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -20,15 +20,25 @@ class Service(models.Model):
 
 
 class Reservation(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='reservations')
-    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='reservations')
-    barber = models.ForeignKey(Employee, on_delete=models.CASCADE, null=True, blank=True, related_name='reservations')
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name="reservations"
+    )
+    service = models.ForeignKey(
+        Service, on_delete=models.CASCADE, related_name="reservations"
+    )
+    barber = models.ForeignKey(
+        Employee,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="reservations",
+    )
 
     date = models.DateField()
     time = models.TimeField()
     status = models.BooleanField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, null=True, default='')
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, null=True, default="")
 
     class Meta:
-        ordering = ['date', 'time']
+        ordering = ["date", "time"]
